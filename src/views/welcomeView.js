b477a98f54b7cfc54e8a903fb795e826492386fb
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2023-09-27 13:36:43 +0200

    change colors to hex, use local logo

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -7,7 +7,7 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
   element.innerHTML = String.raw`
-  <img src="https://1.bp.blogspot.com/-TGQt5uRcAkg/XuMIJoAhwnI/AAAAAAAAA38/FaJQpUUDsGEYR1zBK1wqLWUA9DTYp5CiQCPcBGAYYCw/s587/js.png" alt="Quiz Image" class="quiz-image">
+  <img src="public/js-icon.png" alt="Quiz Image" class="quiz-image">
     <p class='welcome-text'>Welcome To JS QuizUp</p>
     <button id="${START_QUIZ_BUTTON_ID}">START QUIZ</button>
   `;
